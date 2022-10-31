1bf7bc6d30216228763a1017a48f9e6c5d5d7175
Ryan Moore <ryanm.inbox@gmail.com>
2022-10-30 21:27:20 -0400

    add test with `TestClient` and test runner script

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Should return "SELECT FROM_UNIXTIME(1618088028295 / 1000)"
 See https://github.com/tobymao/sqlglot
 """
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 from pydantic import BaseModel
 from sqlglot import transpile
 
@@ -17,3 +18,15 @@ app = FastAPI()
 def transpile_(sql: str, read: str, write: str):
     return transpile(sql=sql, read=read, write=write)[0]
     # return {"sql": sql, "read": read, "write": write}
+
+
+client = TestClient(app)
+
+
+def test_transpile():
+    response = client.get(
+        r"/transpile?sql=SELECT%20EPOCH_MS%281618088028295%29&read=duckdb&write=hive"
+    )
+    print(response)
+    assert response.status_code == 200
+    assert response.json() == "SELECT FROM_UNIXTIME(1618088028295 / 1000)"
